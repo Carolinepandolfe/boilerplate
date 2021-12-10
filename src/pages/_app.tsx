29c25d1fa0532d49d1e856346c0838030de61d07
carolpandolfe <carolinepandolfe@yahoo.com.br>
2021-12-09 21:14:53 -0300

    Add Styles to our Main component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>React avançado</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
